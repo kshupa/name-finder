c578876ed3be8ce3262498c4cb47d84ad4771e8d
christina <>
2021-10-08 13:49:29 -0500

    add test_ner_client.py

--- test/test_ner_client.py
+++ test/test_ner_client.py
@@ -1,4 +1,6 @@
 import unittest
+from ner_client import NamedEntityClient
+
 
 class TestNerClient(unittest.TestCase):
 
@@ -6,6 +8,12 @@ class TestNerClient(unittest.TestCase):
     # html: '<span>>...'}
 
     def test_get_ents_returns_dictionary_given_empty_string(self):
-        ner = NamedEntityClient()
+        model = NerModelTestDouble('eng')
+        ner = NamedEntityClient(model)
         ents = ner.get_ents('')
+        self.assertIsInstance(ents, dict)
+
+    def test_get_ents_returns_list_given_nonempty_string(self):
+        ner = NamedEntityClient(model)
+        ents = ner.get_ents('Autin is the capitol of Texas')
         self.assertIsInstance(ents, dict)
