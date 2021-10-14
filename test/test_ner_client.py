c76444c518071f60c80ac915d9bbcd05921c683c
Khrystyna <>
2021-10-14 17:02:29 -0500

    cleanup env

--- test/test_ner_client.py
+++ test/test_ner_client.py
@@ -1,5 +1,6 @@
 import unittest
 from ner_client import NamedEntityClient
+from test_doubles import NerModelTestDouble
 
 
 class TestNerClient(unittest.TestCase):
@@ -7,13 +8,18 @@ class TestNerClient(unittest.TestCase):
     # {ents: [{...}]},
     # html: '<span>>...'}
 
-    def test_get_ents_returns_dictionary_given_empty_string(self):
+    def test_get_ents_returns_dict_given_empty_string_causes_empty_spacy_doc_ents(self):
         model = NerModelTestDouble("eng")
+        model.returns_doc_ents([])
         ner = NamedEntityClient(model)
         ents = ner.get_ents("")
         self.assertIsInstance(ents, dict)
 
-    def test_get_ents_returns_list_given_nonempty_string(self):
+    def test_get_ents_returns_dict_given_nonempty_string_causes_empty_spacy_doc_ents(
+        self,
+    ):
+        model = NerModelTestDouble("eng")
+        model.returns_doc_ents([])
         ner = NamedEntityClient(model)
         ents = ner.get_ents("Autin is the capitol of Texas")
         self.assertIsInstance(ents, dict)
