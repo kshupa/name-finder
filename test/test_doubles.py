c7a72b9477044846ce8ac8e1b3a4417d00d63db5
Khrystyna <>
2021-10-14 19:49:00 -0500

    add label to NamedEntityClient

--- test/test_doubles.py
+++ test/test_doubles.py
@@ -19,4 +19,21 @@ class DocTestDouble:
     """
 
     def __init__(self, sent, ents):
-        self.ents = [SpanTestDouble(ent["text"])]
+        self.ents = [SpanTestDouble(ent["text"], ent["label_"]) for ent in ents]
+
+    def patch_method(self, attr, return_value):
+        def patched():
+            return return_value
+
+        setattr(self, attr, patched)
+        return self
+
+
+class SpanTestDouble:
+    """
+    Test double for spaCy Span
+    """
+
+    def __init__(self, text, label):
+        self.text = text
+        self.label_ = label
